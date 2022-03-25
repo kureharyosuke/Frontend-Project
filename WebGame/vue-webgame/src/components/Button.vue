84e879d53d340f663a175d5f48294898b40d1bd3
kureharyosuke <ohhayo.com@gmail.com>
2022-03-25 23:21:47 +0900

    #1 install: react & vue

---
+++ WebGame/vue-webgame/src/components/Button.vue
@@ -0,0 +1,11 @@
+<template>
+  <button></button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+};
+</script>
+
+<style></style>
---
+++ WebGame/vue-webgame/src/components/Form.vue
@@ -0,0 +1,11 @@
+<template>
+  <form></form>
+</template>
+
+<script>
+export default {
+  name: "Form",
+};
+</script>
+
+<style></style>
