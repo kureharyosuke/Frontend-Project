19823be08e3684996b3327562de7cb265ce20887
kureharyosuke <ohhayo.com@gmail.com>
2022-03-26 15:43:26 +0900

    #1 add: gugudan

--- WebGame/react-webgame/src/pages/GuGuDan/index.jsx
+++ WebGame/react-webgame/src/pages/GuGuDan/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import PropTypes from "prop-types";
 import Form from "../../components/Form";
 import Input from "../../components/Input";
@@ -7,44 +7,104 @@ import Button from "../../components/Button";
 const GuGuDan = () => {
   const [first, setFirst] = useState(Math.ceil(Math.random() * 9));
   const [second, setSecond] = useState(Math.ceil(Math.random() * 9));
-  // const [number, setNumber] = useState({
-  //   first: Math.ceil(Math.random() * 9),
-  //   second: Math.ceil(Math.random() * 9),
-  // });
-  const [input, setInput] = useState("");
+  const [value, setValue] = useState("");
   const [result, setResult] = useState("");
 
+  const inputRef = useRef(null);
+
+  const [gugudan, setGugudan] = useState({
+    first: Math.ceil(Math.random() * 9),
+    second: Math.ceil(Math.random() * 9),
+    value: "",
+    result: "",
+  });
+
   const onChangeFromInput = (e) => {
-    setInput(e.target.value);
+    setValue(e.target.value);
   };
 
   const onSubmitForm = (e) => {
     e.preventDefault();
     if (first === "" || second === "") {
       alert("두 수를 입력하세요.");
+      inputRef.current.focus();
       return;
-    } else if (first * second === parseInt(input)) {
+    } else if (first * second === parseInt(value)) {
       alert("정답입니다.");
       setResult("정답입니다.");
       setFirst(Math.ceil(Math.random() * 9));
       setSecond(Math.ceil(Math.random() * 9));
-      setInput("");
+      setValue("");
+      inputRef.current.focus();
     } else {
       alert("오답입니다.");
       setResult("오답입니다.");
-      setInput("");
+      setValue("");
+      inputRef.current.focus();
+    }
+  };
+
+  const onChangeFormInput2 = (e) => {
+    setGugudan({
+      ...gugudan,
+      // value: e.target.value,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmitForm2 = (e) => {
+    e.preventDefault();
+    if (gugudan.value === "") {
+      alert("숫자를 입력하세요.");
+      inputRef.current.focus();
+      return;
+    } else if (gugudan.first * gugudan.second === Number(gugudan.value)) {
+      alert("정답입니다.");
+      setGugudan({
+        ...gugudan,
+        first: Math.ceil(Math.random() * 9),
+        second: Math.ceil(Math.random() * 9),
+        result: "정답입니다.",
+        value: "",
+      });
+      inputRef.current.focus();
+    } else {
+      alert("오답입니다.");
+      setGugudan({
+        ...gugudan,
+        result: "오답입니다.",
+        value: "",
+      });
+      inputRef.current.focus();
     }
   };
 
   return (
-    <Form onSubmit={onSubmitForm}>
-      <div>
-        {first} X {second}
-      </div>
-      <Input type={"number"} placeholder={"숫자"} value={input} onChange={onChangeFromInput} />
-      <Button type={"submit"}>submit</Button>
-      {first * second === Number(input) ? <div>{result}</div> : <div>{result}</div>}
-    </Form>
+    <>
+      <Form onSubmit={onSubmitForm}>
+        <div>
+          {first} X {second}
+        </div>
+        <Input inputRef={inputRef} type={"number"} placeholder={"숫자"} value={value} onChange={onChangeFromInput} />
+        <Button type={"submit"}>submit</Button>
+        {first * second === Number(value) ? <div>{result}</div> : <div>{result}</div>}
+      </Form>
+      <Form onSubmit={onSubmitForm2}>
+        <div>
+          {gugudan.first} X {gugudan.second}
+        </div>
+        <Input
+          inputRef={inputRef}
+          type={"number"}
+          placeholder={"숫자"}
+          name={"value"}
+          value={gugudan.value}
+          onChange={onChangeFormInput2}
+        />
+        <Button type={"submit"}>submit</Button>
+        {gugudan.first * gugudan.second === Number(gugudan.value) ? <div>{gugudan.result}</div> : <div>{gugudan.result}</div>}
+      </Form>
+    </>
   );
 };
 
