a18a9cd39067deb8d45e0a63e57bb13d95e7a4aa
kureharyosuke <ohhayo.com@gmail.com>
2022-03-27 21:42:00 +0900

    #2 vue-add: wordlealy gugudan

---
+++ WebGame/vue-webgame/src/components/games/WordLelay.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h2>{{ question }}</h2>
+    <form @submit="onSubmit">
+      <input type="text" v-model="value" placeholder="text please" />
+      <button type="submit">Button</button>
+    </form>
+    <h2>{{ result }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WordLelay",
+  data() {
+    return {
+      question: "apple",
+      value: "",
+      result: "",
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      if (this.question[this.question.length - 1] === this.value[0]) {
+        this.result = "정답";
+        this.question = this.value;
+        this.value = "";
+      } else {
+        this.result = "오답";
+        this.value = "";
+      }
+    },
+    // getRandomWord() {
+    //   const words = ["apple", "banana", "cucumber", "mango", "orange", "pineapple", "strawberry", "watermelon"];
+    //   const randomIndex = Math.floor(Math.random() * words.length);
+    //   return words[randomIndex];
+    // },
+  },
+};
+</script>
+
+<style></style>
---
+++ WebGame/vue-webgame/src/pages/GuGuDan/GuGuDanGame.vue
@@ -0,0 +1,22 @@
+<template>
+  <Form :onClick="onClick">
+    <InputForm />
+  </Form>
+</template>
+
+<script>
+export default {
+  components: {
+    InputForm: () => import("@/components/Input.vue"),
+    Form: () => import("@/components/Form.vue"),
+  },
+  data() {
+    return {
+      first: Math.ceil(Math.random() * 9),
+      second: Math.ceil(Math.random() * 9),
+    };
+  },
+};
+</script>
+
+<style></style>
